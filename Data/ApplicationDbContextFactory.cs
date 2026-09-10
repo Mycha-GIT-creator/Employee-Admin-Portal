4e83eb18cd55f5fc4205b834abfb8546adb7c2ae
Mycha Shem Jimenea <mychasjimenea@gmail.com>
2026-09-10 13:16:20 +0800

    Added frontend

--- Data/ApplicationDbContextFactory.cs
+++ Data/ApplicationDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace Employee_Admin_Portal.Data
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             
             optionsBuilder.UseSqlServer(
-            "Server=localhost;Database=EmployeeAdminPortalDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0"
+            "Server=localhost;Database=EmployeesDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0"
             );
             
             return new ApplicationDbContext(optionsBuilder.Options);
